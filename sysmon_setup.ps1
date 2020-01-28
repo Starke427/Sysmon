4dc847ac961d37e3d168d3e73ac87cfa46d237bb
Starke427 <jstarke@superuser.life>
2020-01-28 13:27:03 -0700

    Update sysmon_setup.ps1

--- sysmon_setup.ps1
+++ sysmon_setup.ps1
@@ -11,7 +11,7 @@ $url2 = "https://raw.githubusercontent.com/Starke427/Sysmon-Configs/master/sysmo
 $file2 = "C:\sysmon_config.xml"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url1, $file1)
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url2, $file2
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url2, $file2)
 
 Expand-Archive -LiteralPath C:\Sysmon.zip -DestinationPath C:\Sysmon
 C:\Sysmon\Sysmon.exe -accepteula -h sha256 -n -l -i C:\sysmon_config.xml
